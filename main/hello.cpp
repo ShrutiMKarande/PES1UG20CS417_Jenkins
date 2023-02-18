842752da9c14ff9c1e6752b0f9a6c0d131dc0b78
ShrutiMKarande <84536294+ShrutiMKarande@users.noreply.github.com>
2023-02-18 16:19:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I added webhook";
-  cout << "Changes have been made";
+  cout << "I added webhook\n";
+  cout << "Changes have been made\n";
   return 0;
 }
